2f6bbe9c1d772e3591d88dc749759372d64ad817
Justin Gray <justin.s.gray@gmail.com>
2026-01-27 23:02:46 -0500

    new_duct compute works

--- pycycle/mp_cycle.py
+++ pycycle/mp_cycle.py
@@ -6,6 +6,7 @@ import openmdao.api as om
 import networkx as nx
 
 from pycycle.element_base import Element
+from pycycle.new_element_base import NewElement
 from pycycle.thermo.cea import species_data
 from pycycle.constants import ALLOWED_THERMOS
 
@@ -60,7 +61,7 @@ class Cycle(om.Group):
 
         self._children[name] = subsys
 
-        if isinstance(subsys, Element): 
+        if isinstance(subsys, (Element, NewElement)):
             self._elements.add(subsys)
             if 'thermo_method' in subsys.options:
                 subsys.options['thermo_method'] = self.options['thermo_method']
