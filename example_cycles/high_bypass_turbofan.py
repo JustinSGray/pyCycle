b69170f851f5ee2799c2c22b375b1cb82ece7c87
Justin Gray <justin.s.gray@gmail.com>
2026-01-29 01:17:03 -0500

    checkpoint. jax derivatives are working and not super slow. Some errors still

--- example_cycles/high_bypass_turbofan.py
+++ example_cycles/high_bypass_turbofan.py
@@ -55,21 +55,21 @@ class HBTF(pyc.Cycle):
         self.add_subsystem('duct4', duct_class())
         self.add_subsystem('lpc', pyc.Compressor(map_data=pyc.LPCMap,
                                         map_extrap=True),promotes_inputs=[('Nmech','LP_Nmech')])
-        self.add_subsystem('duct6', duct_class())
+        self.add_subsystem('duct6', pyc.Duct())
         self.add_subsystem('hpc', pyc.Compressor(map_data=pyc.HPCMap,
                                         bleed_names=['cool1','cool2','cust'], map_extrap=True),promotes_inputs=[('Nmech','HP_Nmech')])
         self.add_subsystem('bld3', pyc.BleedOut(bleed_names=['cool3','cool4']))
         self.add_subsystem('burner', pyc.Combustor(fuel_type=FUEL_TYPE))
         self.add_subsystem('hpt', pyc.Turbine(map_data=pyc.HPTMap,
                                         bleed_names=['cool3','cool4'], map_extrap=True),promotes_inputs=[('Nmech','HP_Nmech')])
-        self.add_subsystem('duct11', duct_class())
+        self.add_subsystem('duct11', pyc.Duct())
         self.add_subsystem('lpt', pyc.Turbine(map_data=pyc.LPTMap,
                                         bleed_names=['cool1','cool2'], map_extrap=True),promotes_inputs=[('Nmech','LP_Nmech')])
-        self.add_subsystem('duct13', duct_class())
+        self.add_subsystem('duct13', pyc.Duct())
         self.add_subsystem('core_nozz', pyc.Nozzle(nozzType='CV', lossCoef='Cv'))
 
         self.add_subsystem('byp_bld', pyc.BleedOut(bleed_names=['bypBld']))
-        self.add_subsystem('duct15', duct_class())
+        self.add_subsystem('duct15', pyc.Duct())
         self.add_subsystem('byp_nozz', pyc.Nozzle(nozzType='CV', lossCoef='Cv'))
 
         #Create shaft instances. Note that LP shaft has 3 ports! => no gearbox
